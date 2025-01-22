c6ea86497179082cf20271a464d74b582c2c9618
Johannes Förstner <johannes@famfoerstner.de>
2025-01-22 18:08:18 +0100

    Update & fix repo scripts

--- build.bat
+++ build.bat
@@ -1,18 +1,14 @@
-:: This script is supposed to be executed from your DS installation folder.
-:: TankCreator and gaspy are expected to be in sibling dirs.
-
 :: name of map
 set map=utraean-peninsula
 :: name of map, case-sensitive
 set map_cs=Utraean Peninsula
-:: path of DSLOA documents dir (where Bits are)
-set doc_dsloa=%USERPROFILE%\Documents\Dungeon Siege LoA
+
+:: path of Bits dir
+set bits=%~dp0.
 :: path of DS installation
-set ds=.
+set ds=%DungeonSiege%
 :: path of TankCreator
-set tc=..\TankCreator
-:: path of GasPy
-set gaspy=..\gaspy
+set tc=%TankCreator%
 
 :: param
 set mode=%1
@@ -20,16 +16,18 @@ echo %mode%
 
 :: Compile map file
 rmdir /S /Q "%tmp%\Bits"
-robocopy "%doc_dsloa%\Bits\world\maps\%map%" "%tmp%\Bits\world\maps\%map%" /E
+robocopy "%bits%\world\maps\%map%" "%tmp%\Bits\world\maps\%map%" /E
 pushd %gaspy%
-venv\Scripts\python -m build.fix_start_positions_required_levels %map% "%tmp%\Bits"
+venv\Scripts\python -m build.fix_start_positions_required_levels %map% --bits "%tmp%\Bits"
 if %errorlevel% neq 0 pause
+setlocal EnableDelayedExpansion
 if not "%mode%"=="light" (
-  venv\Scripts\python -m build.add_world_levels %map% "%tmp%\Bits" "%doc_dsloa%\Bits"
-  if %errorlevel% neq 0 pause
+  venv\Scripts\python -m build.add_world_levels %map% --bits "%tmp%\Bits" --template-bits "%bits%"
+  if !errorlevel! neq 0 pause
 )
+endlocal
 popd
-%tc%\RTC.exe -source "%tmp%\Bits" -out "%ds%\Maps\%map_cs%.dsmap" -copyright "GPG 2002" -title "%map_cs%" -author "Johannes Förstner"
+"%tc%\RTC.exe" -source "%tmp%\Bits" -out "%ds%\Maps\%map_cs%.dsmap" -copyright "GPG 2002" -title "%map_cs%" -author "Johannes Förstner"
 if %errorlevel% neq 0 pause
 
 :: Cleanup
