ad6373d2457f36c2c1f097cabe5c35c9801e4ba9
Johannes Förstner <johannes@famfoerstner.de>
2025-01-22 18:11:48 +0100

    Add a cleanup.bat to cleanup the dsmap

---
+++ cleanup.bat
@@ -0,0 +1,8 @@
+:: name of map, case-sensitive
+set map_cs=Utraean Peninsula
+:: path of DS installation
+set ds=%DungeonSiege%
+
+:: Cleanup resources so as not to confuse Siege Editor
+:: map
+del "%ds%\DSLOA\%map_cs%.dsmap"
